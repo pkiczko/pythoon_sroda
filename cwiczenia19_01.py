5718f877d0fba23b0a0de16ec5c62c2db0048264
Pawel Kiczko <pkiczko@gmail.com>
2022-01-26 18:02:59 +0100

    update final

--- cwiczenia19_01.py
+++ cwiczenia19_01.py
@@ -140,21 +140,27 @@ def wybor_gracza(i):
     if i == 2:
         return "reszka"
     if i == 1:
-        return "orzeł"
+        return "orzeł"    
 
 x = None
+wybor_komputer = None
 wynik = {"gracz":0, "komputer":0}
-while x!=0:
-    
-    menu()
-    x = input("Podaj opcję: ")
-    odliczanie()
-    if random.choice(["orzeł", "reszka"]) == wybor_gracza(x):
-        wynik.gracz += 1
+while x!=0:    
+    menu() #drukowanie menu z funkcji
+    x = int(input("Podaj opcję:")) #należałoby sprawdzić czy wprowadzono cyfrę - by zapobiec błędom
+    print("Wprowadzono: ", x)
+    odliczanie() #opóźnienie 3 sekundowe
+    wybor_komputer = random.choice(["orzeł", "reszka"]) #rejestrowany losowy wybór 
+    print("Wypadł(a): ", wybor_komputer)
+    print("Wybrano: ", wybor_gracza(x))   
+    if wybor_komputer == wybor_gracza(x):
+        wynik["gracz"] += 1
+        print("Gracz dostaje punkt!")
     else:
-        wynik.komputer += 1
+        wynik["komputer"] += 1
+        print("Komputer dostaje punkt!")
     #1den Reszka, 2 orzeł
-
+    
     print("Wynik: ", wynik)
         
 
