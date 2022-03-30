22e65b34147b3e7b98860bc3eff184b0fd272bc0
Pawel Kiczko <pkiczko@gmail.com>
2022-03-30 16:39:26 +0200

    datetime

---
+++ 30marca.py
@@ -0,0 +1,16 @@
+#Bibliotek datetime
+#materiały z  https://github.com/Asabeneh/30-Days-Of-Python/blob/master/16_Day_Python_date_time/16_python_datetime.md
+
+from datetime import datetime
+now = datetime.now()
+print(now)                      # 2021-07-08 07:34:46.549883
+day = now.day                   # 8
+month = now.month               # 7
+year = now.year                 # 2021
+hour = now.hour                 # 7
+minute = now.minute             # 38
+second = now.second
+timestamp = now.timestamp()
+print(day, month, year, hour, minute)
+print('timestamp', timestamp)
+print(f'{day}/{month}/{year}, {hour}:{minute}')  # 8/7/2021, 7:38
